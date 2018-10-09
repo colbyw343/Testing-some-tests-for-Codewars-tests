670ddd130719055aff18e30d2f70a967ffc5f122
Colby <cocoloco@bamafamily.net>
2018-10-09 15:04:08 -0500

    Added links for the corresponding kata on codewars

--- Testing/Kata.cs
+++ Testing/Kata.cs
@@ -8,16 +8,19 @@ namespace Testing
     public class Kata
     {
         //8 8Kyu Kata Methods
+        //https://www.codewars.com/kata/50654ddff44f800200000004
         public int Multiply(int a, int b)
         {
             return a * b;
         }
 
+        //https://www.codewars.com/kata/56dec885c54a926dcd001095
         public int ReverseNum(int a)
         {
             return -a;
         }
 
+        //https://www.codewars.com/kata/583710ccaa6717322c000105
         public int MultiplyBy8or9(int a)
         {
             if(a % 2 == 0)
@@ -29,11 +32,13 @@ namespace Testing
             return d;
         }
 
+        //https://www.codewars.com/kata/53dc23c68a0c93699800041d
         public string SentenceSmash(string[] bleh)
         {
             return string.Join(" ", bleh);
         }
 
+        //https://www.codewars.com/kata/56f6ad906b88de513f000d96
         public string bonus_time(int salary, bool bonus)
         {
             int salaryPro = salary * 10;
@@ -46,6 +51,7 @@ namespace Testing
             return "$" + salaryString;
         }
 
+        //https://www.codewars.com/kata/55ccdf1512938ce3ac000056
         public bool LochNess(string sentence)
         {
             if (sentence.Contains("tree fiddy") || sentence.Contains("tree Fiddy")
@@ -56,18 +62,21 @@ namespace Testing
             return false;
         }
 
+        //https://www.codewars.com/kata/544675c6f971f7399a000e79
         public int StringToNumber(string str)
         {
             int number = int.Parse(str);
             return number;
         }
 
+        //https://www.codewars.com/kata/5265326f5fda8eb1160004c8
         public string NumberToString(int num)
         {
             return num.ToString();
         }
 
         //4 6Kyu Kata Methods
+        //https://www.codewars.com/kata/5208f99aee097e6552000148
         public string BreakCamelCase(string str)
         {
             string words = string.Empty;
@@ -90,12 +99,14 @@ namespace Testing
                 return string.Empty;
         }
 
+        //https://www.codewars.com/kata/523f5d21c841566fde000009
         public int[] ArrayDiff(int[] a, int[] b)
         {
             int[] c = a.Where(x => !b.Contains(x)).ToArray();
             return c;
         }
-        
+
+        //https://www.codewars.com/kata/514b92a657cdc65150000006
         public int Multiples3or5(int value)
         {
             int currentNumber = value;
@@ -119,13 +130,22 @@ namespace Testing
             return allSum;
         }
 
+        //https://www.codewars.com/kata/stop-gninnips-my-sdrow/solutions/csharp
         public string ReverseWords(string sentence)
         {
-            throw new NotImplementedException();
+            string[] strArr = sentence.Split(' ');
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                if (strArr[i].Length >= 5)
+                {
+                    strArr[i] = String.Join("", strArr[i].Reverse());
+                }
+            }
+            return String.Join(" ", strArr);
         }
 
-
-
+        //1 5kyu kata
+        //https://www.codewars.com/kata/513e08acc600c94f01000001
         public string RGBToHex(int r, int g, int b)
         {
                 if (r > 255)
